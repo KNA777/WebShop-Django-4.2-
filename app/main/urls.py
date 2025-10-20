e1dd8de12c5f3934b27609511752d333d31695ef
Nikita <vostrotin.dan@yandex.ru>
2025-10-20 10:45:42 +0300

    add app goods and urls

--- app/main/urls.py
+++ app/main/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
+
     path('', views.index, name='index'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+
 ]
