cd1917675f6c140aae10e467a5c63684a1352788
Nikita <vostrotin.dan@yandex.ru>
2025-10-20 15:18:29 +0300

    add fixtures for goods and some fixies

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
+from goods.models import Categories
+
 
 def index(request):
+    categories = Categories.objects.all()
     context = {
         'title': 'Home',
-        'content': 'Главная страница Web Shop'
+        'content': 'Главная страница Web Shop',
+        'categories': categories,
     }
     return render(request, 'main/index.html', context)
 
