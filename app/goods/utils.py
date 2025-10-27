aba2e736d1397402ecdbb7b2452f740750360e2d
Nikita <vostrotin.dan@yandex.ru>
2025-10-27 12:47:19 +0300

    some fixies

--- app/goods/utils.py
+++ app/goods/utils.py
@@ -1,19 +1,24 @@
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector
 from .models import Products
 
 
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
-        return Products.objects.filter(id=int(query))
+        return Products.objects.annotate(
+            search=SearchVector('name', 'description')).filter(search=query)
 
-    keywords = [word for word in query.split() if len(word) > 2]
+    return Products.objects.filter(description__search=query)
 
-    q_objects = Q()
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
-
-    return Products.objects.filter(q_objects)
+    # keywords = [word for word in query.split() if len(word) > 2]
+    #
+    # q_objects = Q()
+    #
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+    #
+    # return Products.objects.filter(q_objects)
 
 
