0ec73f41e978783b6158e02735509b00c203bf22
Nikita <vostrotin.dan@yandex.ru>
2025-10-27 13:14:50 +0300

    add full text search

--- app/goods/utils.py
+++ app/goods/utils.py
@@ -1,15 +1,16 @@
 from django.db.models import Q
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from .models import Products
 
 
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
-        return Products.objects.annotate(
-            search=SearchVector('name', 'description')).filter(search=query)
+        return Products.objects.filter(id=int(query))
 
-    return Products.objects.filter(description__search=query)
+    vector = SearchVector('name', 'description')
+    query = SearchQuery(query)
 
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by('-rank')
 
     # keywords = [word for word in query.split() if len(word) > 2]
     #
@@ -20,5 +21,3 @@ def q_search(query):
     #     q_objects |= Q(name__icontains=token)
     #
     # return Products.objects.filter(q_objects)
-
-
