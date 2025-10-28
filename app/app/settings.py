d64312f74a6a93d67e89e0437e173ba9355f48ae
Nikita <vostrotin.dan@yandex.ru>
2025-10-28 15:17:18 +0300

    full text search fixies

--- app/app/settings.py
+++ app/app/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
 
     'main',
     'goods',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -41,7 +42,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
